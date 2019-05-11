e0ca921addf78a7c7c7357791bad2547a84097c3
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2019-05-11 17:45:02 +0200

    Events update

--- JS/events.js
+++ JS/events.js
@@ -7,8 +7,8 @@ $.ajax({
       $(data.events).each(function (index, value) {
         // document.getElementById("h1").innerHTML = value.title;
         //document.getElementById("demo1").innerHTML = value.image;
-        var record = "<tr><td><div> <img src=" + value.image + " alt= ' ' id='img_event'> <div class'container' id='all_text' ><h1>" + value.title + " </h1><h2>" +
-          value.date + "</h2><p>" + value.description + "</p></div></div></td></tr>";
+        var record = "<tr><td><div> <img src=" + value.image + " alt= ' ' id='img_event'> <div class'container' id='all_text' ><h1 class='book-title'>" + value.title + " </h1><h2 class='book-details'>" +
+          value.date + "</h2><p class='book-description'>" + value.description + "</p></div></div></td></tr>";
         $("table").append(record);
       });
     }
