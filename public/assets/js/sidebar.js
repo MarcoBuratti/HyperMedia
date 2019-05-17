94bba7ee14280d59c1e9105426a43c708df09d53
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2019-05-17 13:29:11 +0200

    book page

--- public/assets/js/sidebar.js
+++ public/assets/js/sidebar.js
@@ -1,3 +1,8 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 // Get input element
 let btnOpen = document.getElementById('sidebar-btn-open');
 // Add Event Listener
@@ -18,3 +23,56 @@ function closeSlideMenu() {
     document.getElementById('main').style.marginLeft = '0';
     document.getElementById('main').style.opacity = '1.0';
 }
+
+// Get EVENT button element
+let btnEvent = document.getElementById('sidebar-btn-event');
+
+// Add EventListener to EVENT button
+btnEvent.addEventListener("click", openEventPage);
+
+// Open Event Page
+function openEventPage() {
+    resetButtons();
+    activeButton(btnEvent);
+}
+
+let btnBook = document.getElementById('sidebar-btn-info');
+
+btnBook.addEventListener("click", openBookPage);
+
+function openBookPage() {
+    resetButtons();
+    activeButton(btnBook);
+}
+
+let btnGallery = document.getElementById('sidebar-btn-gallery');
+
+btnGallery.addEventListener("click", openGalleryPage);
+
+function openGalleryPage() {
+    resetButtons();
+    activeButton(btnGallery);
+}
+
+let btnReviews = document.getElementById('sidebar-btn-reviews');
+
+btnReviews.addEventListener("click", openReviewsPage);
+
+function openReviewsPage() {
+    resetButtons();
+    activeButton(btnReviews);
+}
+
+// Select Button
+function activeButton(btn) {
+    btn.classList.add('active-sidebar-btn');
+}
+
+// Reset Buttons
+function resetButtons() {
+    var sidebarBtns = document.getElementsByClassName("sidebar-btn");
+    for ( var i = 0; i < sidebarBtns.length ; i++ ) {
+        sidebarBtns[i].classList.remove('active-sidebar-btn');
+    }
+}
+
