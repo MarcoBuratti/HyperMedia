6c36afd952b26ad06600c0fe20964a97bb77d3c2
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2019-05-25 15:25:53 +0200

    db seeds update

--- db/seeds/04_books.js
+++ db/seeds/04_books.js
@@ -5,9 +5,13 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('books').insert([
-        { isbn: '0', title: 'Book 0', theme1: 'theme1 0', theme2: 'theme2 0', genre1: 'genre1 0', genre2: 'genre2 0', date: '0/0/0000', recommended: true, price: '10.5', status: 'available' },
-        { isbn: '1', title: 'Book 1', theme1: 'theme1 1', theme2: 'theme2 1', genre1: 'genre1 1', genre2: ' genre2 1', date: '1/11/1111', recommended: true, price: '10.5', status: 'available' },
-        { isbn: '2', title: 'Book 2', theme1: 'theme1 2', theme2: 'theme2 2', genre1: 'genre1 2', genre2: 'genre2 2', date: '2/22/2222', recommended: false, price: '10.5', status: 'available' }
+        { isbn: '88-389-1017-0', title: "La forma dell'acqua", theme1: 'Police', theme2: '-', genre1: 'Crime Novel', genre2: 'Mystery Novel', date: '10-03-1994', recommended: true, price: '6.80', status: 'available' },
+        { isbn: '88-389-1226-2', title: 'Il cane di terracotta', theme1: 'Police', theme2: '-', genre1: 'Crime Novel', genre2: 'Mystery Novel', date: '18-04-1996', recommended: true, price: '6.80', status: 'available' },
+        { isbn: '88-7782-702-5', title: 'Harry Potter e la pietra filosofale', theme1: 'Magic', theme2: 'Teenagers', genre1: 'Fantasy Novel', genre2: 'Young Adult Novel', date: '26-06-1997', recommended: false, price: '8.50', status: 'available' },
+        { isbn: '88-7782-703-3', title: 'Harry Potter e la camera dei segreti', theme1: 'Magic', theme2: 'Teenagers', genre1: 'Fantasy Novel', genre2: 'Young Adult Novel', date: '02-07-1998', recommended: false, price: '8.50', status: 'unavailable' },
+        { isbn: '88-11-58093-5', title: 'Cime Tempestose', theme1: 'Love', theme2: 'Revenge', genre1: 'Tragedy', genre2: 'Gothic Novel', date: '12-1847', recommended: true, price: '5.90', status: 'available' },
+        { isbn: '978-88-090-3371-9', title: 'Orgoglio e Pregiudizio', theme1: 'Love', theme2: 'Class', genre1: 'Romantic Novel', genre2: 'Historical Fiction', date: '28-01-1813', recommended: true, price: '7.40', status: 'unavailable' },
+        
       ]);
     });
 };
