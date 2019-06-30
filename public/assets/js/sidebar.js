8b4148726d29e6815738f1cb90ccbdc96258b080
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2019-06-30 17:38:34 +0200

    Update

--- public/assets/js/sidebar.js
+++ public/assets/js/sidebar.js
@@ -6,6 +6,7 @@ let json;
 
 
 let buyForm = document.getElementById('buy-form');
+let authors = document.getElementById('book-authors');
 let title = document.getElementById('book-title');
 let isbn = document.getElementById('isbn-code');
 let cover = document.getElementById('book-cover');
@@ -30,22 +31,162 @@ const userAction = async () => {
   
   userAction();
   
-  function loadData(json) {
-      title.innerHTML = json[0].title;
-      isbn.innerHTML = "ISBN: " + json[0].isbn;
-      var genreHTML = genres.innerHTML;
-      genreHTML = genreHTML + "Literary Genres: " + json[0].genre1;
-      if (json[0].genre2 !== "-")
-          genreHTML = genreHTML + ", " + json[0].genre2;
-      genres.innerHTML = genreHTML;
-      var themeHTML = themes.innerHTML;
-      themeHTML = themeHTML + "Themes: " + json[0].theme1;
-      if (json[0].theme2 !== "-")
-          themeHTML = themeHTML + ", " + json[0].theme2;  
-      themes.innerHTML = themeHTML;
-      price.innerHTML = "Price: " + json[0].price.toFixed(2) + "€";
-      cover.src = "../assets/img/books/" + json[0].isbn + ".jpg";
-      descr.innerHTML = json[0].descr;
+async function loadData(json) {
+
+    let response = await fetch('../../v2/authorsByIsbn/' + json[0].isbn);
+    author = await response.json();
+    let authorsHTML;
+    title.innerHTML = json[0].title;
+    isbn.innerHTML = "ISBN: " + json[0].isbn;
+    var genreHTML = genres.innerHTML;
+    genreHTML = genreHTML + "Literary Genres: " + json[0].genre1;
+    if (json[0].genre2 !== "-")
+        genreHTML = genreHTML + ", " + json[0].genre2;
+    genres.innerHTML = genreHTML;
+    var themeHTML = themes.innerHTML;
+    themeHTML = themeHTML + "Themes: " + json[0].theme1;
+    if (json[0].theme2 !== "-")
+        themeHTML = themeHTML + ", " + json[0].theme2;  
+    themes.innerHTML = themeHTML;
+    price.innerHTML = "Price: " + json[0].price.toFixed(2) + "€";
+    cover.src = "../assets/img/books/" + json[0].isbn + ".jpg";
+    descr.innerHTML = "Preface: " + json[0].descr;
+
+    if (author.length > 1) {
+      authorsHTML =  "Authors: " + author[0].name + ", ";
+      for(var j=1; j<author.length-1; j++) {
+          authorsHTML += author[j].name + ", ";
+      }
+      authorsHTML += author[author.length-1].name;
+    }
+    else {
+        authorsHTML = "Author: " + author[0].name;
+    }
+    authors.innerHTML = authorsHTML;
+
+
+    let similar = [];
+    let content;
+
+    for( var i = 0 ; i<authors.length; i++) {
+      response = await fetch('../../v2/booksByIdAuthor/' + authors[i].id_author);
+      content = await response.json();
+      if (content !== undefined) {
+        for (i = 0; i<content.length; i++) {
+          similar.push(content[i]);
+
+        }
+      }
+    }
+    response = await fetch('../../v2/bookGenre/' + json[0].genre1);
+    content = await response.json();
+    if (content !== undefined) {
+      for (i = 0; i<content.length; i++) {
+        similar.push(content[i]);
+
+      }
+    }
+    if (json[0].genre2 !== '-') {
+      response = await fetch('../../v2/bookGenre/' + json[0].genre2);
+      content = await response.json();
+      if (content !== undefined) {
+        for (i = 0; i<content.length; i++) {
+          similar.push(content[i]);
+
+        }
+      }
+    }
+    response = await fetch('../../v2/bookTheme/' + json[0].theme1);
+    content = await response.json();
+    if (content !== undefined) {
+      for (i = 0; i<content.length; i++) {
+        similar.push(content[i]);
+
+      }
+    }
+    if (json[0].theme2 !== '-'){
+      response = await fetch('../../v2/bookTheme/' + json[0].theme2);
+      content = await response.json();
+      if (content !== undefined) {
+        for (i = 0; i<content.length; i++) {
+          similar.push(content[i]);
+        }
+      }
+    }
+
+    var unique = uniqueArray(similar);
+
+
+
+  if (unique.length === 0) {
+    response = await fetch('../../v2/books');
+    content = await response.json();
+
+/*
+    for (var i = content.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = content[i];
+      content[i] = content[j];
+      content[j] = temp;
+    }*/
+
+    var len = content.length;
+    len--;
+    var j = 0;
+    i = Math.floor(Math.random() * (len));
+    console.log(i);
+    while (j < 3) {
+      if ( content[i].isbn !== json[0].isbn ) {
+        unique.push(content[i]);
+        j++;
+      }
+      i = Math.floor(Math.random() * (len));
+    }
+    console.log(unique);
+  }
+
+  let galleryInd = document.getElementById('gallery-indicators');
+  let galleryElems = document.getElementById('gallery-elems');
+  
+  let carouselIndHTML = '<li data-target="#myCarousel" data-slide-to="0"></li>';
+  let carouselElemHTML = ' <div class="item active"><img class="image" src="../assets/img/books/' + unique[0].isbn + '.jpg" id="img-carousel"><div class="carousel-caption"><h3>' + unique[0].title +'</h3><a class="btn btn-default btn-sm showcase-btn" href="">Read More</a></div></div>';
+  for (i=1; i<unique.length; i++) {
+    carouselIndHTML += '<li data-target="#myCarousel" data-slide-to="' + i + '"></li>';
+    carouselElemHTML += ' <div class="item"><img class="image" src="../assets/img/books/' + unique[i].isbn + '.jpg" id="img-carousel"><div class="carousel-caption"><h3>' + unique[i].title +'</h3><a class="btn btn-default btn-sm showcase-btn" href="">Read More</a></div></div>';
+  }
+  galleryInd.innerHTML += carouselIndHTML;
+  galleryElems.innerHTML = carouselElemHTML + galleryElems.innerHTML;
+
+}
+
+  function uniqueArray(similar) {
+    var newArr = [],
+        origLen = similar.length,
+        found, x, y;
+    for (x = 0; x < origLen; x++) {
+        found = undefined;
+        if (similar[x].isbn !== json[0].isbn) {
+          for (y = 0; y < newArr.length; y++) {
+              if (similar[x].isbn === newArr[y].isbn) {
+                  found = true;
+                  break;
+              }
+          }
+          if (!found) {
+              newArr.push(similar[x]);
+          }
+        }
+    }
+    return newArr;
+  }
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
   }
   
   
