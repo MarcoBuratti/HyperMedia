e389ac953c8f1e5a828177b2062f5840717cf315
ChristianCammareri <38110773+ChristianCammareri@users.noreply.github.com>
2019-07-03 21:41:36 +0200

    .

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
         directory: __dirname + '/db/migrations',
       },
       seeds: {
-        directory: __dirname + '/db/seeds/production',
+        directory: __dirname + '/db/seeds',
       },
     }
   };
