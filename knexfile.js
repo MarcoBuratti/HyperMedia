f923e571d9a6294e80978cfe662463d3b9b05076
ChristianCammareri <38110773+ChristianCammareri@users.noreply.github.com>
2019-07-03 21:09:21 +0200

    .

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     development: {
       client: 'pg',
       
-      connection: 'postres://postgres:Sergio@localhost/testdue',
+      connection: 'postgres://aouhgoinjndwvn:687dfce950901be9b7fca25bbdf716269952e5bcba60976d83c1e5fc8efbad6a@ec2-54-75-224-168.eu-west-1.compute.amazonaws.com:5432/dbmpatb09amckh',
 
       migrations: {
         directory: __dirname + '/db/migrations',
